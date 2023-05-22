83c3bcac1a66c5dba283f94dd5fe9255c67549a5
Lilu Cao <lilu.cao@orbitfin.ai>
2023-05-22 09:03:34 +0800

    add orbit DateEncoder

--- orbitkit/util/util_date.py
+++ orbitkit/util/util_date.py
@@ -1,4 +1,15 @@
 import datetime
+import json
+
+
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%dT%H:%M:%S%z')
+        elif isinstance(obj, datetime.date):
+            return obj.strftime("%Y-%m-%d")
+        else:
+            return json.JSONEncoder.default(self, obj)
 
 
 def get_date_range_list_v1(start_date, end_date):
