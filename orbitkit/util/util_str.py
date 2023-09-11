96da9a307752ad2041c79052a962e62dab6dfc5d
Lilu Cao <lilu.cao@orbitfin.ai>
2023-09-11 16:02:45 +0800

    Add get_value() method in str util

--- orbitkit/util/util_str.py
+++ orbitkit/util/util_str.py
@@ -1,9 +1,30 @@
+from typing import Dict
+
 EMPTY_STR = ""
 
 
-def is_empty(word):
+def is_empty(word: str):
     return bool(word == EMPTY_STR)
 
 
-def is_empty_strip(word):
-    return bool(str(word).strip() == EMPTY_STR)
+def is_empty_strip(word: str):
+    return bool(word.strip() == EMPTY_STR)
+
+
+def get_value(item: Dict, key: str):
+    """
+    If key not in item:
+        - then return ""
+    If key in item:
+        - If None then return ""
+        - If not None then return item[key]
+
+    :param item:
+    :param key:
+    :return:
+    """
+    if key not in item:
+        return ""
+    if item[key] is None:
+        return ""
+    return item[key]
